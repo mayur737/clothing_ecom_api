ba508a36d9a104675569ad92716da9178976db63
DESKTOP-H7VHGT1\ADmin <nikhil.d@cheriseglobal.com>
2025-11-07 09:32:16 +0530

    changes

--- controllers/user/product.controller.js
+++ controllers/user/product.controller.js
@@ -76,4 +76,82 @@ const addProductToCart = async (res, res, next) => {
     }
 };
 
-module.exports = { getProductDetails, getProductList, addProductToCart, addProductToWishList }
+const removeProductfromCart = async (req, res, next) => {
+    try {
+        const userId = req.user.id;
+        const { id } = req.params;
+        const cart = await Cart.findOne({ userId });
+        if (!cart) {
+            return res.status(404).json({ message: "Cart not found" });
+        }
+        const index = cart.products.indexOf(id);
+        if (index === -1) {
+            return res.status(404).json({ message: "Product not found in cart" });
+        }
+        cart.products.splice(index, 1);
+        await cart.save();
+
+        res.status(200).json({ message: "Product removed from cart successfully" });
+    } catch (error) {
+        console.error(error);
+        next({ st: 500, ms: error.message });
+    }
+};
+
+
+const removeProductFromWishlist = async (req, res, next) => {
+    try {
+        const userId = req.user.id;
+        const { id } = req.params;
+        const wishlist = await WishList.findOne({ userId });
+        if (!wishlist) {
+            return res.status(404).json({ message: "WishList not found" });
+        }
+        const index = wishlist.products.indexOf(id);
+        if (index === -1) {
+            return res.status(404).json({ message: "Product not found in wishlist" });
+        }
+        wishlist.products.splice(index, 1);
+        await wishlist.save();
+
+        res.status(200).json({ message: "Product removed from wishlist successfully" });
+    } catch (error) {
+        console.error(error);
+        next({ st: 500, ms: error.message });
+    }
+};
+
+const moveProductFromWishlistToCart = async (req, res, next) => {
+    try {
+        const userId = req.user.id;
+        const { id } = req.params;
+        const wishlist = await WishList.findOne({ userId });
+        if (!wishlist) {
+            return res.status(404).json({ message: "WishList not found" });
+        }
+        const index = wishlist.products.indexOf(id);
+        if (index === -1) {
+            return res.status(404).json({ message: "Product not found in wishlist" });
+        }
+        wishlist.products.splice(index, 1);
+        await wishlist.save();
+
+        const cart = await Cart.findOne({ userId });
+        if (!cart) {
+            await Cart.create({
+                userId: id,
+                products: []
+            })
+        }
+        if (id && !cart.products.includes(id)) {
+            cart.products.push(id);
+        }
+        await cart.save();
+        res.status(200).json({ data: { message: "Product added to cart successfully" } })
+    } catch (error) {
+        console.log(error);
+        next({ st: 500, ms: error.message })
+    }
+};
+
+module.exports = { getProductDetails, getProductList, addProductToCart, addProductToWishList, removeProductFromWishlist, removeProductfromCart, moveProductFromWishlistToCart }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { getProductList, getProductDetails, addProductToWishList, addProductToCart } = require("../controllers/user/product.controller");
+const { getProductList, getProductDetails, addProductToWishList, addProductToCart, removeProductfromCart, removeProductFromWishlist, moveProductFromWishlistToCart } = require("../controllers/user/product.controller");
 
 const router = express.Router();
 
 router.get("/product-list", getProductList);
 router.get("/product/:id", getProductDetails);
-router.post("/add/wishlist", addProductToWishList);
-router.post("/add/cart", addProductToCart);
+router.post("/add//:id", addProductToWishList);
+router.post("/add/cart/:id", addProductToCart);
+router.post("/remove/cart/:id", removeProductfromCart);
+router.post("/remove/wishlist/:id", removeProductFromWishlist);
+router.post("/move/cart/:id", moveProductFromWishlistToCart);
 
 module.exports = router;
